c024c41dec4c43ebfa01184bf8bbd6ca1d68cff3
Alexander Zaytsev <alexander@say26.com>
2015-09-17 17:20:57 +0500

    pew

--- lib/colorscore/histogram.rb
+++ lib/colorscore/histogram.rb
@@ -1,7 +1,7 @@
 module Colorscore
   class Histogram
-    def initialize(image_path, colors=16, depth=8)
-      output = `convert #{image_path} -format %c -dither None -quantize YUV -colors #{colors} -depth #{depth} histogram:info:-`
+    def initialize(image_path, colors=16)
+      output = `convert #{image_path} -format %c -dither None -quantize YUV -colors #{colors} -depth 8 histogram:info:-`
       @lines = output.lines.sort.reverse.map(&:strip).reject(&:empty?)
     end
 
