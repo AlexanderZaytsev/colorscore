0b37707d0b9623e4bb694ef02222562af14b27e4
Alexander Zaytsev <alexander@say26.com>
2015-09-17 17:51:21 +0500

    pew

--- lib/colorscore/histogram.rb
+++ lib/colorscore/histogram.rb
@@ -1,18 +1,20 @@
 module Colorscore
   class Histogram
     def initialize(image_path, colors=16)
-      output = `convert #{image_path} -format %c -dither None -quantize YUV -colors #{colors} -depth 8 -alpha off histogram:info:-`
-      @lines = output.lines.sort.reverse.map(&:strip).reject(&:empty?)
+      output = `convert #{image_path} -format %c -dither None -quantize YUV -colors #{colors} -depth 8 -alpha on histogram:info:-`
+      @lines = output.lines.sort.reverse.map(&:strip).reject(&:empty?).reject do |line|
+        line =~ /srgba\([0-9]+,[0-9]+,[0-9]+,0\)/
+      end
     end
 
     # Returns an array of colors in descending order of occurances.
     def colors
-      hex_values = @lines.map { |line| line[/#([0-9A-F]{6}) /, 1] }.compact
+      hex_values = @lines.map { |line| line[/#([0-9A-F]{6})/, 1] }.compact
       hex_values.map { |hex| Colour::RGB.from_html(hex) }
     end
 
     def color_counts
-      @lines.select { |line| line =~ /#([0-9A-F]{6}) / }.map { |line| line.split(':')[0].to_i }
+      @lines.map { |line| line.split(':')[0].to_i }
     end
 
     def scores
